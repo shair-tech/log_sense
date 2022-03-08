2d2f9398697f4c3e35a6c69aab824edf2c489cb1
Adolfo Villafiorita <adolfo.villafiorita@ict4g.net>
2022-03-08 11:12:21 +0100

    fixes IP parsing + rubocop-ified

--- lib/log_sense/ip_locator.rb
+++ lib/log_sense/ip_locator.rb
@@ -4,19 +4,22 @@ require 'ipaddr'
 require 'iso_country_codes'
 
 module LogSense
+  #
+  # Populate table of IP Locations from dbip-country-lite
+  #
   module IpLocator
-    DB_FILE = File.join(File.dirname(__FILE__), "..", "..", "ip_locations", "dbip-country-lite.sqlite3")
+    DB_FILE = File.join(File.dirname(__FILE__), '..', '..', 'ip_locations', 'dbip-country-lite.sqlite3')
 
-    def self.dbip_to_sqlite db_location
-      db = SQLite3::Database.new ":memory:"
-      db.execute "CREATE TABLE ip_location (
+    def self.dbip_to_sqlite(db_location)
+      db = SQLite3::Database.new ':memory:'
+      db.execute 'CREATE TABLE ip_location (
         from_ip_n INTEGER,
         from_ip TEXT,
         to_ip TEXT,
         country_code TEXT
-      )"
+      )'
 
-      ins = db.prepare "INSERT INTO ip_location(from_ip_n, from_ip, to_ip, country_code) values (?, ?, ?, ?)"
+      ins = db.prepare 'INSERT INTO ip_location(from_ip_n, from_ip, to_ip, country_code) values (?, ?, ?, ?)'
       CSV.foreach(db_location) do |row|
         ip = IPAddr.new row[0]
         ins.execute(ip.to_i, row[0], row[1], row[2])
@@ -33,14 +36,16 @@ module LogSense
       SQLite3::Database.new DB_FILE
     end
 
-    def self.locate_ip ip, db
-      return if not ip
+    def self.locate_ip(ip, db)
+      return unless ip
 
-      ip_n = IPAddr.new(ip).to_i
-      res = db.execute "SELECT * FROM ip_location where from_ip_n <= #{ip_n} order by from_ip_n desc limit 1"
       begin
+        ip_n = IPAddr.new(ip).to_i
+        res = db.execute "SELECT * FROM ip_location where from_ip_n <= #{ip_n} order by from_ip_n desc limit 1"
         IsoCountryCodes.find(res[0][3]).name
-      rescue
+      rescue IPAddr::InvalidAddressError
+        'INVALID IP'
+      rescue IsoCountryCodes::UnknownCodeError
         res[0][3]
       end
     end
@@ -48,14 +53,14 @@ module LogSense
     #
     # add country code to data[:ips]
     #
-    def self.geolocate data
-      @location_db = IpLocator::load_db
+    def self.geolocate(data)
+      @location_db = IpLocator.load_db
+
       data[:ips].each do |ip|
-        country_code = IpLocator::locate_ip ip[0], @location_db
+        country_code = IpLocator.locate_ip ip[0], @location_db
         ip << country_code
       end
       data
     end
-
   end
 end
