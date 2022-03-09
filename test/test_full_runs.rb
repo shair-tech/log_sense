8df3c5495185b13bba6b78aebe2ebc2960f8830c
Adolfo Villafiorita <adolfo.villafiorita@ict4g.net>
2022-03-09 09:59:00 +0100

    version number in reports

--- test/test_full_runs.rb
+++ test/test_full_runs.rb
@@ -5,11 +5,11 @@ require 'log_sense.rb'
 class FullRunTest < Minitest::Test
   def test_full_run
     output = { 'apache' => 'sample_logs/short.log', 'rails' => 'sample_logs/emas.log' }
-    output.each do |format, file|
+    output.each do |input_format, input_file|
       %w[html txt].each do |output_format|
-        cli = "--input-format=#{format} --input-files=#{file} --output-format=#{output_format}"
+        cli = "--input-format=#{input_format} --input-files=#{input_file} --output-format=#{output_format}"
         puts "Running: #{cli}"
-        `ruby -Ilib exe/log_sense #{cli}`
+        `ruby -Ilib exe/log_sense #{cli} > test-#{input_format}.#{output_format}`
       end
     end
   end
