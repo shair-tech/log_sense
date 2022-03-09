e79c252614b0438902af47da0a9958179c29fe18
Adolfo Villafiorita <adolfo.villafiorita@ict4g.net>
2022-03-09 09:11:31 +0100

    fixes to test cases (can you believe it? ;-))

--- test/test_full_runs.rb
+++ test/test_full_runs.rb
@@ -4,12 +4,10 @@ require 'log_sense.rb'
 
 class FullRunTest < Minitest::Test
   def test_full_run
-    output = [{ 'apache' => 'sample_logs/short.log' },
-              { 'rails'  => 'sample_logs/emas.log' }]
-
+    output = { 'apache' => 'sample_logs/short.log', 'rails' => 'sample_logs/emas.log' }
     output.each do |format, file|
-      %w[html text].each do |output_format|
-        cli = "--input-format #{format} --input-files=#{file} --output-format #{output_format}"
+      %w[html txt].each do |output_format|
+        cli = "--input-format=#{format} --input-files=#{file} --output-format=#{output_format}"
         puts "Running: #{cli}"
         `ruby -Ilib exe/log_sense #{cli}`
       end
