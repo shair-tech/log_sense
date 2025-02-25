d7361e2411687931b2390b22af8a194a7c382fff
Adolfo Villafiorita <adolfo@shair.tech>
2025-02-25 15:14:34 +0100

    refactoring

--- lib/log_sense/apache/log_line_parser.rb
+++ lib/log_sense/apache/log_line_parser.rb
@@ -21,33 +21,33 @@ module LogSense
     # 116.179.32.16 - - [19/Dec/2021:22:35:11 +0100] "GET / HTTP/1.1" 200 135 "-" "Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html)"
     #
     class LogLineParser
-      DAY = /[0-9]{2}/
-      MONTH = /[A-Za-z]{3}/
-      YEAR = /[0-9]{4}/
-      TIMEC = /[0-9]{2}/
-      TIMEZONE = /(\+|-)[0-9]{4}/
+      DAY = "[0-9]{2}"
+      MONTH = "[A-Za-z]{3}"
+      YEAR = "[0-9]{4}"
+      TIMEC = "[0-9]{2}"
+      TIMEZONE = "(\\+|-)[0-9]{4}"
 
-      IP = /(?<ip>[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|::1|unknown)/
-      IDENT = /(?<ident>[^ ]+|-)/
-      USERID = /(?<userid>[^ ]+|-)/
+      IP = "(?<ip>[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}|::1|unknown)"
+      IDENT = "(?<ident>[^ ]+|-)"
+      USERID = "(?<userid>[^ ]+|-)"
 
-      TIMESTAMP = /(?<date>#{DAY}\/#{MONTH}\/#{YEAR}):(?<time>#{TIMEC}:#{TIMEC}:#{TIMEC} #{TIMEZONE})/
+      TIMESTAMP = "(?<date>#{DAY}\\/#{MONTH}\\/#{YEAR}):(?<time>#{TIMEC}:#{TIMEC}:#{TIMEC} #{TIMEZONE})"
 
-      HTTP_METHODS = /GET|HEAD|POST|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH/
-      WEBDAV_METHODS = /COPY|LOCK|MKCOL|MOVE|PROPFIND|PROPPATCH|UNLOCK/
-      OTHER_METHODS = /SEARCH|REPORT|PRI|HEAD\/robots.txt/
-      METHOD = /(?<method>#{HTTP_METHODS}|#{WEBDAV_METHODS}|#{OTHER_METHODS})/
-      PROTOCOL = /(?<protocol>HTTP\/[0-9]\.[0-9]|-|.*)/
-      URL = /(?<url>[^ ]+)/
-      REFERER = /(?<referer>[^"]*)/
-      RETURN_CODE = /(?<status>[1-5][0-9][0-9])/
-      SIZE = /(?<size>[0-9]+|-)/
-      USER_AGENT = /(?<user_agent>[^"]*)/
+      HTTP_METHODS = "GET|HEAD|POST|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH"
+      WEBDAV_METHODS = "COPY|LOCK|MKCOL|MOVE|PROPFIND|PROPPATCH|UNLOCK"
+      OTHER_METHODS = "SEARCH|REPORT|PRI|HEAD/robots.txt"
+      METHOD = "(?<method>#{HTTP_METHODS}|#{WEBDAV_METHODS}|#{OTHER_METHODS})"
+      PROTOCOL = "(?<protocol>HTTP\/[0-9]\.[0-9]|-|.*)"
+      URL = "(?<url>[^ ]+)"
+      REFERER = '(?<referer>[^"]*)'
+      RETURN_CODE = "(?<status>[1-5][0-9][0-9])"
+      SIZE = "(?<size>[0-9]+|-)"
+      USER_AGENT = '(?<user_agent>[^"]*)'
 
       attr_reader :format
 
       def initialize
-        @format = /#{IP} #{IDENT} #{USERID} \[#{TIMESTAMP}\] "(#{METHOD} #{URL} #{PROTOCOL}|-|.+)" #{RETURN_CODE} #{SIZE} "#{REFERER}" "#{USER_AGENT}"/
+        @format = /#{IP} #{IDENT} #{USERID} \[#{TIMESTAMP}\] "(#{METHOD} #{URL} #{PROTOCOL}|-|.+)" #{RETURN_CODE} #{SIZE} "#{REFERER}" "#{USER_AGENT}"/o
       end
 
       def parse(line)
