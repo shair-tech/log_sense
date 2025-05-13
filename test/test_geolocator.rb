6462f364ea9d8efeba607efad290ea4cadfd56bc
Adolfo Villafiorita <adolfo@shair.tech>
2025-05-13 11:49:39 +0200

    rubocop + update calls

--- test/test_geolocator.rb
+++ test/test_geolocator.rb
@@ -1,13 +1,13 @@
-require 'minitest/autorun'
+require "minitest/autorun"
 
-require 'log_sense.rb'
+require "log_sense.rb"
 
 class DataCruncherTest < Minitest::Test
   # not really a test, more of a script to check what goes on
   def test_geolocation
-    input_files = [File.open('sample_logs/spmbook_com.log', 'r')]
+    input_files = [File.open("sample_logs/spmbook_com.log", "r")]
 
-    parser = LogSense::ApacheLogParser.new
+    parser = LogSense::Apache::LogParser.new
     db = parser.parse input_files
 
     aggregator = LogSense::ApacheAggregator.new(db, { limit: 30 })
@@ -15,8 +15,8 @@ class DataCruncherTest < Minitest::Test
     
     data = LogSense::IpLocator.geolocate data
 
-    assert_equal 'United States of America', data[:ips][0][4], 'Maybe GeoIp changed?'
-    assert_equal 'United States of America', data[:ips][1][4], 'Maybe GeoIp changed?'
-    assert_equal 'Palestine, State of', data[:ips].last[4], 'Maybe GeoIp changed?'
+    assert_equal "United States of America", data[0][4], "Maybe GeoIp changed?"
+    assert_equal "United States of America", data[1][4], "Maybe GeoIp changed?"
+    assert_equal "Palestine, State of", data.last[4], "Maybe GeoIp changed?"
   end
 end
